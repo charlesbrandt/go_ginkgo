b0b49dc7652a7f98e1ea2558e798b2b6fc66f54d
Charles Brandt <charles.brandt@gmail.com>
2014-08-01 07:50:04 -0400

    updating todo, launch and instance notes

--- launch.sh
+++ launch.sh
@@ -5,5 +5,14 @@ python /c/mindstream/mindstream/launch.py -c $ROOT todo
 
 echo "Other common options:
 python /c/mindstream/mindstream/launch.py -c $ROOT code
+
+python /c/mindstream/mindstream/launch.py -c /c/technical yeoman
+
+to start development:
+gulp watch
+
+to build for deployment:
+gulp
+
 "
 
